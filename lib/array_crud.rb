50254e9aa8cd18fbbaf2ca405f0cad765fdf96a0
Maika Kailani <queenmab051@gmail.com>
2019-10-23 00:20:15 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -40,8 +40,8 @@ def retrieve_element_from_index(array, index_number)
 end
 
 def retrieve_first_element_from_array(array)
-  planets[0]
-  
+  planets["Manaan"]
+  p planets[0]
 end
 
 def retrieve_last_element_from_array(array)
