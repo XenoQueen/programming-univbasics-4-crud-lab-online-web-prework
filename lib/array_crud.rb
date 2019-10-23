ed8da834c9e574458d10285d239377dfd2b5438d
Maika Kailani <queenmab051@gmail.com>
2019-10-23 00:07:08 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -7,12 +7,12 @@ def create_an_array
 end
 
 def add_element_to_end_of_array(array, element)
-  planets << ["Korriban"]
+  planets << ("Korriban")
   p planets
 end1
 
 def add_element_to_start_of_array(array, element)
-  planets.unshift ["Nar Shadaa"]
+  planets.unshift ("Nar Shadaa")
   p planets
 end
 
